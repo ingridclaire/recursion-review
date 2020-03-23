c1a9d9c4fff33e3a7e4225b2b1138accee2188b5
Ingrid Carlson <ingridclaire@gmail.com>
2020-03-23 15:36:11 -0700

    if statement for numbers

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,10 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  let result = '';
+  let addToString = function(thing) {
+    if ( typeof thing === 'number') {
+      result += `${thing}`;
+    }
+  };
 };
